2ae1bdedca798dc223c30d98873090df5852ff85
Yamez <jcook7104@sohamcollege.org.uk>
2014-05-18 14:26:22 +0100

    Update grid.py
    Here is my version of the code

--- grid.py
+++ grid.py
@@ -1,4 +1,9 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+#My grid code#
+#below i have defined the word grid to start the code#
+def grid():
+    for x in range(5):
+        print("-------")
+        print("| | | |")
+
+#i have used a for loop to ma\ke the grid bigger#
+#Code by James Cook#
